cd6df5022af95d3dcfeacd2be357f1bd40b9162e
ClarkTheShark <osborneclark5@gmail.om>
2026-01-13 09:44:44 -0500

    Followed the lesson, made undertale

---
+++ Assets/Scripts/LinearInterploationIG.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class LinearInterploationIG : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+    public float t;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        t += 0.5f*Time.deltaTime;
+        if(t >= 1)
+        {
+            t = 0;
+        }
+        transform.position = Vector2.Lerp(start.position, end.position, t);
+    }
+}
