f8772d87b19773bf66671fdc9ad9230964312d6f
ClarkTheShark <osborneclark5@gmail.om>
2026-01-13 10:14:05 -0500

    Made some funky Animation Curves

--- Assets/Scripts/LinearInterploationIG.cs
+++ Assets/Scripts/LinearInterploationIG.cs
@@ -5,6 +5,7 @@ public class LinearInterploationIG : MonoBehaviour
     public Transform start;
     public Transform end;
     public float t;
+    public AnimationCurve curve;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,6 +22,7 @@ public class LinearInterploationIG : MonoBehaviour
         {
             t = 0;
         }
-        transform.position = Vector2.Lerp(start.position, end.position, t);
+        //transform.position = Vector2.Lerp(start.position, end.position, t);
+        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
     }
 }
